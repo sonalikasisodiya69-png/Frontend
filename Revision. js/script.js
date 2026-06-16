5b47a428596e32db7fc8f92dfd3281948b908a21
Sonalika <sonalikasisodiya69@gmail.com>
2026-06-16 16:51:52 +0530

    first

---
+++ Revision. js/script.js
@@ -0,0 +1,18 @@
+function calculator(a,b,operator) {
+    switch(operator) {
+        case "+":
+        return a + b;
+
+        case "-":
+            return a - b;
+             case "*":
+      return a * b;
+
+    case "/":
+      return a / b;
+
+    default:
+        return "Invalid operator";
+    }
+}
+console.log(calculator(5,4,"*"));
